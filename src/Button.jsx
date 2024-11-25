470fb564a5160f9259ac0c408530af71c59933f0
Jadonamite <jadonamite@gmail.com>
2024-11-25 16:49:57 +0100

    A lot on listOnReact

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,27 +1,11 @@
-// To style the button there are different ways this could be done 
-// 1. External
-// 2. Module
-// 3. Inline
-function Button(){
-const styles = {
-        display: "block",
-        backgroundColor:" aquamarine",
-        color: "rgb(62, 41, 6)",
-        padding: "10px 20px",
-        borderRadius: "5px",
-        border: "none",
-        cursor: "pointer",
-       }
-    return(
-        <button style={styles}>
-            Click Me
-            
-        </button>
+function Button() {
+   //     handleClick
 
-    );
+   //    return <button onClick={}>Click Me</button>;
+
+   setTimeout(function () {
+      console.log("Hello");
+   }, 3000);
 }
 
-
-
-
-export default Button
+export default Button;
