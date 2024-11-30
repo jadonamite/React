a85eba367a558d0a144efd5b12c4a3d254feb58c
Jadonamite <jadonamite@gmail.com>
2024-11-30 23:59:10 +0100

    click events in react

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,13 +1,19 @@
+import React, { useState } from "react";
+
 function Button() {
-   const handleClick = () => console.log("I'll change it for you");
-   const handleClick2 = (name) =>
-      console.log(`${name} I've warned you to stop clicking me`);
+   const [count, setCount] = useState(0);
 
-   return <button onClick={() => handleClick2("jadonamite")}>Click Me</button>;
+   const handleClick = (name) => {
+      if (count <= 3) {
+         setCount(count + 1);
+         console.log(`${name}, you have clicked me ${count + 1} times`);
+      } else {
+         console.log(`${name}, if you click me the ${count + 1}th time`);
+         setCount(count + 1);
+      }
+   };
 
-   //    setTimeout(function () {
-   //       console.log("Hello");
-   //    }, 3000);
+   return <button onClick={() => handleClick("jadonamite")}>Click Me</button>;
 }
 
 export default Button;
