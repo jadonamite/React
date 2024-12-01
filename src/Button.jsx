0e01d52604e79c390f78cb76349e200d67cb2637
Jadonamite <jadonamite@gmail.com>
2024-12-01 13:17:51 +0100

    click events in react: Onclicks OnDoubleClicks and e for Events

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 
 function Button() {
-   const [count, setCount] = useState(0);
+   // const [count, setCount] = useState(0);
 
-   const handleClick = (name) => {
-      if (count <= 3) {
-         setCount(count + 1);
-         console.log(`${name}, you have clicked me ${count + 1} times`);
-      } else {
-         console.log(`${name}, if you click me the ${count + 1}th time`);
-         setCount(count + 1);
-      }
-   };
+   // const handleClick = (name) => {
+   //    if (count <= 3) {
+   //       setCount(count + 1);
+   //       console.log(`${name}, you have clicked me ${count + 1} times`);
+   //    } else {
+   //       console.log(`${name}, if you click me the ${count + 1}th time`);
+   //       setCount(count + 1);
+   //    }
+   // };
 
-   return <button onClick={() => handleClick("jadonamite")}>Click Me</button>;
+   // return <button onClick={() => handleClick("jadonamite")}>Click Me</button>;
+
+   // Now lets not be more generic and lets work with the events
+
+   const handleClickAgain = (e) => (e.target.textContent = "Hey Stop it");
+
+   return (
+      <button onDoubleClick={(event) => handleClickAgain(event)}>
+         Click Me
+      </button>
+   );
 }
+
+// the e stands for event (that is the event hanndler ) which can be written in short as e
+
+// I can also use instead of an onclick a onDoubleClick for double clicking events as the snippet below
+
+// return (
+//    <button onDoubleClick={(event) => handleClickAgain(event)}>Click Me</button>
+// );
 
 export default Button;
