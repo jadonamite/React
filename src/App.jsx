2b5106942bd565dd572150572fc70285b98472ba
Jadonamite <jadonamite@gmail.com>
2024-12-17 11:01:54 +0100

    Updater functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import Button from "./Button.jsx";
 import MyComponent from "./MyComponent.jsx";
 import Counter from "./Counter.jsx";
 import ColorPicker from "./ColorPicker.jsx";
+import UptCounter from "./UpdateFunctions.jsx";
 
 function App() {
-   return <ColorPicker />;
+   return <UptCounter />;
 }
 export default App;
