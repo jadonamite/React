e1f2f8958adb9053db4d0435bbaa28c218e2d9ec
Jadonamite <jadonamite@gmail.com>
2024-12-24 20:07:05 +0100

    Crafting a To-do-list with gotten knowledge

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,10 +1,86 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-   const [tasks, setTasks] = useState([]);
-   const [newTasks, setTasks] = useState([]);
+   const [tasks, setTasks] = useState(["Buy food", "Take  a bath"]);
+   const [newTask, setNewTask] = useState([]);
 
-   return <div></div>;
+   function handleInputChange(e) {
+      setNewTask(e.target.value);
+   }
+   function addTask(e) {
+      if (newTask !== "") {
+         setTasks([...tasks, newTask]);
+         setNewTask("");
+      } else {
+         !alert("You haven't added any task");
+      }
+   }
+
+   function deleteTask(index) {
+      const updatedTask = tasks.filter((_, i) => i !== index);
+      setTasks(updatedTask);
+   }
+   function moveUp(index) {
+      if (index > 0) {
+         const updatedTask = [...tasks];
+         [updatedTask[index], updatedTask[index - 1]] = [
+            updatedTask[index - 1],
+            updatedTask[index],
+         ];
+         setTasks(updatedTask);
+      }
+   }
+   function moveDown(index) {
+      if (index < tasks.length - 1) {
+         const updatedTask = [...tasks];
+         [updatedTask[index + 1], updatedTask[index]] = [
+            updatedTask[index],
+            updatedTask[index + 1],
+         ];
+         setTasks(updatedTask);
+      }
+   }
+   // function moveDown(index) {
+   //    if (index < tasks.length - 1) {
+   //       const updatedTask = [...tasks];
+   //       [updatedTask[index], updatedTask[index + 1]] = [
+   //          updatedTask[index + 1],
+   //          updatedTask[index],
+   //       ];
+   //       setTasks(updatedTask);
+   //    }
+   // }
+
+   return (
+      <div className="to-do-list">
+         <h1 className="header">Plan your Day</h1>
+         <ol>
+            {tasks.map((task, index) => (
+               <li key={index} className="todo-list">
+                  <span className="task">{task}</span>
+                  <button className="del-btn" onClick={() => deleteTask(index)}>
+                     Delete
+                  </button>
+                  <button className="up-btn" onClick={() => moveUp(index)}>
+                     Take up
+                  </button>
+                  <button className="down-btn" onClick={() => moveDown(index)}>
+                     Take down
+                  </button>
+               </li>
+            ))}
+         </ol>
+         <input
+            type="text"
+            value={newTask}
+            placeholder="Enter a new task"
+            onChange={handleInputChange}
+         />
+         <button className="add-btn" onClick={addTask}>
+            Add
+         </button>
+      </div>
+   );
 }
 
 export default ToDoList;
