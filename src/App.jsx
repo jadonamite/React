0e028fafd7ac0f78bc89d4623b49da684fc994c2
Jadonamite <jadonamite@gmail.com>
2024-12-25 07:04:18 +0100

    Understanding  useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import ToDoList from "./ToDoList.jsx";
+import MyComponent from "./useEffects.jsx";
 
 function App() {
-   return <ToDoList />;
+   return (
+      <>
+         <ToDoList />
+         <MyComponent />
+      </>
+   );
 }
 export default App;
