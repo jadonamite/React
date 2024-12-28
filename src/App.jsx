2fd4d88aed33b2ccf9f0787235b973c54c64a387
Jadonamite <jadonamite@gmail.com>
2024-12-28 16:53:46 +0100

    Using useEffect in a different approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import DigitalClock from "./DigitalClock.jsx";
+import DigitalClock from "./DigitalClock/digitalClock.jsx";
 import Dimension from "./return.jsx";
 import ToDoList from "./ToDoList.jsx";
 import MyComponent from "./useEffects.jsx";
